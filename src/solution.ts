af8ae16657efd137e23cc32b48316c943c34578f
tawhid <ahmedshanto690@gmail.com>
2025-11-18 01:57:04 +0600

    Add filterActiveUsers function and implement printBookDetails for Book interface

--- src/solution.ts
+++ src/solution.ts
@@ -54,3 +54,42 @@ function filterByRating(items: { title: string; rating: number }[]): {
 // ];
 
 // console.log(filterByRating(books));
+
+function filterActiveUsers(
+  users: { name: string; email: string; isActive: boolean }[]
+): { name: string; email: string; isActive: boolean }[] {
+  return users.filter(user => user.isActive === true);
+}
+
+// const users = [
+//   { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+//   { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+//   { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+// ];
+
+// console.log(filterActiveUsers(users));
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book): void {
+  const availability = book.isAvailable ? 'Yes' : 'No';
+
+  console.log(`Title: ${book.title}`);
+  console.log(`Author: ${book.author}`);
+  console.log(`Published Year: ${book.publishedYear}`);
+  console.log(`Available: ${availability}`);
+}
+
+const myBook: Book = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+printBookDetails(myBook);
