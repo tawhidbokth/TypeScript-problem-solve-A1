b337a1635424692afc037e9fe33618415f2c248c
tawhid <ahmedshanto690@gmail.com>
2025-11-18 01:14:06 +0600

    Implement formatValue

--- src/solution.ts
+++ src/solution.ts
@@ -1,2 +1,27 @@
-const test = 'This is a test string.';
-console.log(test);
+function formatValue(
+  value: string | number | boolean
+): string | number | boolean {
+  if (typeof value === 'string') {
+    return value.toUpperCase();
+  }
+  if (typeof value === 'number') {
+    return value * 10;
+  }
+  if (typeof value === 'boolean') {
+    return !value;
+  }
+
+  return value;
+}
+
+function getLength(value: string | any[]): number {
+  if (typeof value === 'string') {
+    return value.length;
+  }
+
+  if (Array.isArray(value)) {
+    return value.length;
+  }
+
+  return 0;
+}
