f6e8a15ba872032ba2ac03f6ca50561f537f4db5
tawhid <ahmedshanto690@gmail.com>
2025-11-18 01:22:18 +0600

    Add Person class with constructor and getDetails method

--- src/solution.ts
+++ src/solution.ts
@@ -25,3 +25,17 @@ function getLength(value: string | any[]): number {
 
   return 0;
 }
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name} Age: ${this.age}`;
+  }
+}
